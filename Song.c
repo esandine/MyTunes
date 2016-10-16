32fe9f8751de723495ec356f377e1c016fb3aca0
esandine <esandine@stuy.edu>
2016-10-16 16:36:56 -0400

    Random and remove work

--- Song.c
+++ Song.c
@@ -8,10 +8,13 @@ typedef struct node{
   struct node *next;
 } node;
 
-
+char print_node(struct node* n){
+    printf("Name: %s\nArtist: %s\n\n", n->name, n->artist);
+    return 1;
+}
 char print_list(struct node* n){
   if(n){
-    printf("Name: %s\nArtist: %s\n\n", n->name, n->artist);
+    print_node(n);
     return print_list(n->next);
   }
   return 1;
@@ -54,6 +57,37 @@ struct node* findArtist(struct node* front, char* artist){
   return 0;
 }
 
+int len(node* front){
+  int i = 0;
+  while(front){
+    i++;
+    front=front->next;
+  }
+  return i;
+}
+
+struct node* random_node(node* front){
+  int i = rand()%len(front);
+  while(i){
+    front=front->next;
+    i--;
+  }
+  return front;
+}
+
+char remove_node(node* front, node* rem){
+  if(front==rem){
+    front=front->next;
+    free(rem);
+  }else{
+    while(!(front->next==rem)){
+      front=front->next;
+    }
+    front->next=rem->next;
+    free(rem);
+  }
+  return 1;
+}
 int main(){
   node* s1 = 0;
   s1=insert_front(s1, "elysong", "elyname");
@@ -63,6 +97,19 @@ int main(){
   s1=insert(s1, "clysong2", "elyname5");
   print_list(s1);
   printf("findSong(s1,blysong2)->name: %s\n\n",findSong(s1,"blysong2")->name);
-  printf("findSong(s1,elyname4)->name: %s\n\n",findArtist(s1,"elyname4")->name);
+  printf("(s1,elyname4)->name: %s\n\n",findArtist(s1,"elyname4")->name);
+  printf("len(s1): %d\n",len(s1));
+  printf("random_node(s1): \n");
+  print_node(random_node(s1));
+  printf("random_node(s1): \n");
+  print_node(random_node(s1));
+  printf("random_node(s1): \n");
+  print_node(random_node(s1));
+  remove_node(s1, findSong(s1, "blysong2"));
+  printf("remove_node(s1, findSong(s1, blysong2))\n");
+  print_list(s1);
+  
+
+  
   return 0;
 }
