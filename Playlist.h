8d85991b15bae60cdc56a6ebad466f377b4b8152
esandine <esandine@stuy.edu>
2016-10-20 11:02:01 -0400

    Not quite work

--- Playlist.h
+++ Playlist.h
@@ -1,6 +1,13 @@
 #ifndef PLAYLIST
 #include "Song.h"
 #define PLAYLIST
-
-
+node Playlist[26]
+char add_song(char* name, char* artist);
+node* find_song(char* name);
+node* find_song(char* artist);
+char print_letter(char letter);
+char print_artist(char* artist);
+char shuffle();
+char delete(char* name);
+char delete();
 #endif
